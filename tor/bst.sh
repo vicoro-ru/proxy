18cdefaebfbadbff6cc63a59eb3dd80283052b61
Михаил <alisterosenheim@outlook.com>
2026-01-19 17:05:11 +0300

    Add Webtunel List

--- tor/bst.sh
+++ tor/bst.sh
@@ -1,13 +1,24 @@
 #!/bin/sh
-set -e
-
-# Сколько мостов нужно выбрать
+#set -e
 COUNT="${BRIDGES_COUNT:-3}"
 
-# Чистим рабочий конфиг и копируем пример
+# 1. Восстанавливаем базу конфига из шаблона
 cp /etc/tor/torrc.example /etc/tor/torrc
 
-# Выбираем случайные строки из torList и дописываем в torrc
-shuf -n "$COUNT" /etc/tor/torList | sed 's/^/Bridge /' >> /etc/tor/torrc
+echo "" >> /etc/tor/torrc
+echo "# --- Динамически добавленные мосты ---" >> /etc/tor/torrc
+
+# 2. Выбираем obfs4 мосты
+if [ -f /etc/tor/obfs4.list ]; then
+    shuf -n "$COUNT" /etc/tor/obfs4.list | sed 's/^/Bridge /' >> /etc/tor/torrc
+    echo "[$(date)] Добавлено $COUNT мостов obfs4"
+fi
+
+# 3. Выбираем WebTunnel мосты
+if [ -f /etc/tor/webtunnel.list ]; then
+    shuf -n "$COUNT" /etc/tor/webtunnel.list | sed 's/^/Bridge /' >> /etc/tor/torrc
+    echo "[$(date)] Добавлено $COUNT мостов webtunnel"
+fi
+
+# 4. Устанавливаем права
 chown tor:tor /etc/tor/torrc
-echo "[$(date)] Добавлено $COUNT мост(ов) в /etc/tor/torrc"
